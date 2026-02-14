21b8cac55f01fb65ab586405a4862fd216647d22
Anuja Annavarapu <aannavar@terpmail.umd.edu>
2026-02-14 08:40:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const button = document.getElementById('no-button');
+button.addEventListener('mouseover', function () {
+    button.style.position = "absolute";
+    button.style.left = `${Math.ceil(Math.random() * 90)}%`;
+    button.style.top = `${Math.ceil(Math.random() * 90)}%`;
+});
+
+
+const firstPage = document.getElementById('first-page');
+const secondPage = document.getElementById('second-page');
+const yesButton = document.getElementById('yes-button');
+yesButton.addEventListener('click', function () {
+    firstPage.style.display = "none";
+    secondPage.style.display = "flex";
+});
+
+const nextButton = document.getElementById('next')
+if (nextButton) {
+  nextButton.addEventListener("click", function () {
+    window.location.href = "/password";
+  });
+}
+
